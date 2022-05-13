d09a97fce40b4d0f4aa2cf7772c3600ace6bdd83
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-05-13 19:28:20 +0545

    Time: 2 ms (42.71%), Space: 6.5 MB (44.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int mod=int(1e9+7);
-    int tocheck(int m ,int n,vector<vector<int>>&dp){
-   for(int i=1;i<m;i++){
-       for(int j=1;j<n;j++){
-           dp[i][j]=dp[i-1][j]+dp[i][j-1];
-       }
-   }
-        return dp[m-1][n-1];
-    }
+   
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,1));
-      return   tocheck(m,n,dp);
+ 
+      vector<vector<int>>dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
