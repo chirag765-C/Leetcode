1043616f38226214f9894eccb90ad472cb00ecc3
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-05-28 19:38:48 +0545

    Time: 193 ms (78.33%), Space: 60.9 MB (66.21%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>>ans(n+1,vector<int>(n+1,0));
-        for(auto &v:trust){
-            ans[v[0]][v[1]]=1;
+        if(trust.size()==0 && n==1){
+            return 1;
         }
-        stack<int>st;
-        for(int i=1;i<=n;i++){
-            st.push(i);
+      vector<int>in(n+1,0);
+        vector<int>out(n+1,0);
+        for(auto &edge:trust){
+            out[edge[0]]++;
+            in[edge[1]]++;
         }
-        while(st.size()>=2){
-            int i=st.top();
-            st.pop();
-            int j=st.top();
-            st.pop();
-            if(ans[i][j]==1){
-                //i knows j
-                st.push(j);
-            }
-            else{
-                st.push(i);
+        for(int i=0;i<=n;i++){
+            if(out[i]==0 && in[i]==n-1){
+                return i;
             }
         }
-        int pot=st.top();
-        st.pop();
-        int c=1;
-        for(int i=1;i<=n;i++){
-            if(i!=pot){
-                if(ans[i][pot]==0 || ans[pot][i]==1){
-                    return -1;
-                }
-            }
-        }
-        return pot;
-        
+        return -1;
         
     }
 };
