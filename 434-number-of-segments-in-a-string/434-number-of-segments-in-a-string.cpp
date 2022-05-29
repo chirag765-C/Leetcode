0635ca9f2fe9c322179724b671eb335d90fa3f0c
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-05-29 21:31:27 +0545

    Time: 0 ms (100.00%), Space: 6.2 MB (10.14%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     int countSegments(string s) {
-        if(s.size()==0){
-            return 0;
-        }
-        stack<string>st;
-        int cnt=0;
-                   string t="";
-
-        for(int i=0;i<s.size();i++){
-            if(!isspace(s[i])){
-                t+=s[i];
-            }
-            else{
-                if(t!=""){
-                st.push(t);
-                }
-                t="";
-            }
-            if(i==s.size()-1){
-                if(t!=""){
-                st.push(t);
-                }
+    int cnt=0;
+        s+=' ';
+        for(int i=1;i<s.size();i++){
+            if(s[i]==' ' && s[i-1]!=' '){
+                cnt++;
             }
         }
-        return st.size();
+        return cnt;
     }
 };
