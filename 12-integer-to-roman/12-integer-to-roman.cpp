c742a91bbedccafd21a551bb33154c3e54cc77e3
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-05-30 23:00:19 +0545

    Time: 11 ms (58.02%), Space: 6.1 MB (51.22%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans="";
+        int  val[13]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string rom[13]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;num;i++){
+            while(num>=val[i]){
+                ans+=rom[i];
+                num=num-val[i];
+            }
+        }
+        return ans;
+    }
+};
