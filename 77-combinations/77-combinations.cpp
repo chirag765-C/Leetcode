6f810a4be3bc522344579adb980bf91bc6388ca8
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-07-10 09:53:35 +0545

    Time: 33 ms (69.09%), Space: 9.9 MB (59.65%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void f(int i,int n,int k,vector<int>&v){
-    if(v.size()==k){
-        ans.push_back(v);
-        return;
-    }
-        for(int ind=i;ind<=n;ind++){
-            v.push_back(ind);
-           
-            f(ind+1,n,k,v);
+   vector<vector<int>>ans;
+    void tofind(int i,int n,int k,vector<int>&v){
+        if(v.size()==k){
+            ans.push_back(v);
+            return;
+        }
+        for(int j=i;j<=n;j++){
+            v.push_back(j);
+            tofind(j+1,n,k,v);
             v.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-      vector<int>nums;
-        for(int i=1;i<=n;i++){
-            nums.push_back(i);
-        }
-        vector<int>v;
-         f(1,n,k,v);
+    vector<int>v;
+        tofind(1,n,k,v);
         return ans;
-    }
+    } 
 };
