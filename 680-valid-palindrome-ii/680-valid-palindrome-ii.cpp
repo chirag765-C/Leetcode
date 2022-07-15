fbc0fa3388be68f52c2b118c7f4893583bf426ba
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-07-15 09:37:52 +0545

    Time: 43 ms (98.82%), Space: 24.1 MB (33.95%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
+    
+    
+    bool isvalid(string s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
     bool validPalindrome(string s) {
         
-        int cnt1=0,cnt2=0;
-     int i=0;
-     int j=s.size()-1;
+int i=0;
+        int j=s.size()-1;
         while(i<j){
             if(s[i]!=s[j]){
-                cnt1++;
-                i++;
+                return isvalid(s,i+1,j) || isvalid(s,i,j-1);
             }
-           else{
             i++;
             j--;
-           }
-             if(cnt1>1){
-                break;
-            }
         }
-i=0;
-        j=s.size()-1;
-        while(i<j){
-            if(s[i]!=s[j]){
-                cnt2++;
-                j--;
-            }
-           else{
-            i++;
-            j--;
-           }
-             if(cnt2>1){
-                break;
-            }
-        }
-        if(cnt1==1 || cnt2==1){
-            return true;
-        }
-        else if(cnt1==0 and cnt2==0){
-           return true; 
-        }
-        return false;
+        return true;
     }
+    
 };
