e86a2b796012c20308d606f65543e6215c0b9e82
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-07-15 09:25:17 +0545

    Added solution - LeetHub

--- Police and Thieves - GFG/police-and-thieves.cpp
+++ Police and Thieves - GFG/police-and-thieves.cpp
@@ -13,31 +13,33 @@ class Solution{
     int catchThieves(char arr[], int n, int k) 
     {
         // Code here 
-        vector<int>police,thief;
-        for(int i=0;i<n;i++){
-            if(arr[i]=='P'){
-                police.push_back(i);
-            }
-            else if(arr[i]=='T'){
-                thief.push_back(i);
-            }
-        }
-        int ans=0;
-        int i=0,j=0;
-        while(i<police.size() && j<thief.size()){
-            if(abs(police[i]-thief[j])<=k){
-                ans++;
-                i++;
-                j++;
-            }
-            else if(police[i]>thief[j]){
-                j++;
-            }
-            else if(police[i]<thief[j]){
-                i++;
-            }
-        }
-        return ans;
+      vector<int>p;
+      vector<int>t;
+      for(int i=0;i<n;i++){
+          if(arr[i]=='P'){
+              p.push_back(i);
+          }
+          else if(arr[i]=='T'){
+              t.push_back(i);
+          }
+      }
+      int i=0,j=0;
+      int cnt=0;
+     while(i<p.size() and j<t.size()){
+         if(abs(p[i]-t[j])<=k){
+             cnt++;
+             i++;
+             j++;
+         }
+         else if(p[i]>t[j]){
+             j++;
+         }
+         else if(p[i]<t[j]){
+             i++;
+         }
+     }
+     return cnt;
+        
     }
 };
 
