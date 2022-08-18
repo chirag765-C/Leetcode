cf591051878bd69a6709b061ed3f110373a40ad5
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-08-18 23:48:32 +0530

    Time: 3 ms (44.60%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int mod=int(1e9+7);
-   
+  int f(int i,int j,int m,int n,vector<vector<int>>&dp){
+      if(i==m-1 and j==n-1){
+          return 1;
+      }
+      if(i>=m || j>=n){
+          return 0;
+      }
+      if(dp[i][j]!=-1){
+          return dp[i][j];
+      }
+      int down=f(i+1,j,m,n,dp);
+      int right=f(i,j+1,m,n,dp);
+      return dp[i][j]= down+right;
+  }
     int uniquePaths(int m, int n) {
- 
-      vector<vector<int>>dp(m,vector<int>(n,1));
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-             
-                
-                int down,right;
-                if(i>0){
-                    down=dp[i-1][j];
-                }
-                if(j>0){
-                    right=dp[i][j-1];
-                }
-                dp[i][j]=down+right;
-            }
-        }
-        return abs(dp[m-1][n-1]);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(0,0,m,n,dp);
     }
 };
