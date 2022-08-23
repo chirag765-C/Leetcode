d51ce42c4e4578a64037c86b171ab2cff1225f53
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-08-23 19:04:07 +0530

    Time: 148 ms (15.34%), Space: 27.2 MB (28.94%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int tocheck(string &x ,string &y,int m,vector<vector<int>>&dp){
+  
+    int f(string x,string y,int m,vector<vector<int>>&dp){
         for(int i=0;i<m+1;i++){
-            dp[0][i]=0;
             dp[i][0]=0;
+            dp[0][i]=0;
+            
         }
-        int size=-1;
         for(int i=1;i<m+1;i++){
             for(int j=1;j<m+1;j++){
                 if(x[i-1]==y[j-1]){
@@ -15,17 +16,15 @@ public:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            }
-     return dp[m][m];
-   
+        }
+        return dp[m][m];
     }
     int minInsertions(string s) {
-      string y=s;
+    int m=s.size();
+        string y=s;
         reverse(y.begin(),y.end());
-        int m=s.size();
         vector<vector<int>>dp(m+1,vector<int>(m+1,-1));
-        int x= tocheck(s,y,m,dp);
-        x=m-x;
-        return x;
+        int x=f(s,y,m,dp);
+        return m-x;
     }
 };
