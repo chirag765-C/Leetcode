e797c54fd1e95046715e9b7838e7d4133e056130
chirag765-C <85434885+chirag765-C@users.noreply.github.com>
2022-09-07 19:09:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (76.76%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-   string decode(string &s,int &i){
-       string result;
-       while(i<s.size() and s[i]!=']'){
-           if(isdigit(s[i]) ){
-                          int k=0;
-
-               while(i<s.size() and isdigit(s[i])){
-                     k=k*10+s[i++]-'0';
-                   
-               }
-               //starting bracker
-               i++;
-               string r=decode(s,i);
-               while(k-->0){
-                   result+=r;
-                   
-                   
-               }
-               
-             // for last bracket
-               i++;
-           }
-           else{
-               result+=s[i++];
-               
-           }
-       }
-       return result;
-   }
+  
+  string decode(string &s,int &i){
+      string result;
+      while(i<s.size() and s[i]!=']'){
+          if(isdigit(s[i])){
+                int k=0;
+              while(i<s.size() and isdigit(s[i])){
+                  k=k*10+s[i]-'0';
+                  i++;
+              }
+              i++;
+              string r=decode(s,i);
+              while(k--){
+                  result+=r;
+              }
+              i++;
+                
+          }
+             else{
+                 result+=s[i];
+                 i++;
+             }
+      }
+             return result;
+  }
     string decodeString(string s) {
       int i=0;
         return decode(s,i);
